4cd87b63001a59260ba967dcc3351601f9ac5f87
michh99 <michelangelofernando47@gmail.com>
2025-03-14 10:05:17 +0100

    ri installaz react vite e tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import Navigation from './components/Navigation'
+import './App.css'
+import Card from './components/card'
+import { Header } from './components/header'
 
 function App() {
 
-
   return (
-    <div>
-      <Navigation/>  
+    <>
+      <Header/>
+      <Card/>
+      <h1 className='bg-gray-300'>Vite + React</h1>
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
 
-    </div>
-    
+    </>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+export const Header = () => {
+    return(
+        <header className="header">
+            <img src="" alt="" />
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Prodotti</li>
+                    <li>Servizi</li>
+                    <li>Contatti</li>
+                
+                </ul>
+            </nav>
+
+        </header>
+    )
+
+
+}
